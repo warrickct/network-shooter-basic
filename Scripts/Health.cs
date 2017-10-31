ca67e19a5a4c73fde726ac81d894bbefb52a5892
warrickct <warrickctjunk@gmail.com>
2017-10-31 15:59:15 +1300

    Part way through the enemy spawning stage of the networking tutorial

---
+++ Scripts/Health.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class Health : NetworkBehaviour
+{
+
+    public const int maxHealth = 100;
+
+    [SyncVar(hook = "OnChangeHealth")]
+    public int currentHealth = maxHealth;
+    public RectTransform healthBar;
+
+    public void TakeDamage(int amount)
+    {
+
+        if (!isServer)
+        {
+            return;
+        }
+
+        currentHealth -= amount;
+        if (currentHealth <= 0)
+        {
+            currentHealth = maxHealth;
+
+            //called on server but invoked on the clients.
+            //necessary because client has local authority on player obj, not the server.
+            RpcRespawn();
+        }
+    }
+
+    void OnChangeHealth (int currentHealth)
+    {
+        healthBar.sizeDelta = new Vector2(currentHealth, healthBar.sizeDelta.y);
+    }
+
+    [ClientRpc]
+    void RpcRespawn()
+    {
+        if (isLocalPlayer)
+        {
+            //move back to zero location
+            transform.position = Vector3.zero;
+        }
+    }
+}
---
+++ Scripts/PlayerController.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+
+//To make network aware
+using UnityEngine.Networking;
+
+
+//For network aware change mono to networkbehaviour
+public class PlayerController : NetworkBehaviour
+{
+
+    public GameObject bulletPrefab; //ref for the bullet object
+    public Transform bulletSpawn;   //reference to the location of the bullet spawn
+
+    void Update()
+    {
+        //For networking - Will exit out the update before executing anything important if it's not the local player and connection
+        if (!isLocalPlayer)
+        {
+            return;
+        }
+        var x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
+        var z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
+
+        transform.Rotate(0, x, 0);
+        transform.Translate(0, 0, z);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            CmdFire();
+        }
+    }
+
+    public override void OnStartLocalPlayer()
+    {
+        GetComponent<MeshRenderer>().material.color = Color.blue;
+    }
+
+    [Command]
+    void CmdFire()
+    {
+        //Create a bullet from the bullet prefab
+        var bullet = (GameObject)Instantiate(bulletPrefab, bulletSpawn.position, bulletSpawn.rotation);
+
+        //Add velocity to the bullet
+        bullet.GetComponent<Rigidbody>().velocity = bullet.transform.forward * 6;
+
+        //Spawn the bullet on the clients
+        NetworkServer.Spawn(bullet);
+
+        //Destroy bullet after 2 seconds
+        Destroy(bullet, 2.0f);
+    }
+}
